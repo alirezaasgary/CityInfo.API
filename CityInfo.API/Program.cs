157e2b61db60927b86f8281da32ef17bc1d37013
alireza asgary <asgary_alireza@hotmail.com>
2022-12-13 02:43:40 +0330

    Pipeline config

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -10,16 +10,17 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+
+#region Pipline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.Run(async (contex) => {
+    await contex.Response.WriteAsync("helllollllooooo");
+});
+#endregion
 
 app.Run();
