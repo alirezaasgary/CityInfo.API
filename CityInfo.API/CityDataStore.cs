74e1556adfe7f4b73327a31c8773053ea41e8f63
alireza asgary <asgary_alireza@hotmail.com>
2022-12-13 18:49:21 +0330

    Add intrested model

--- CityInfo.API/CityDataStore.cs
+++ CityInfo.API/CityDataStore.cs
@@ -11,8 +11,20 @@ namespace CityInfo.API
         {
             Cities = new List<CityDto>()
             {
-                new CityDto() {Id=1,Name="Moscow",Description="" },
-                new CityDto() {Id=2,Name="Tehran",Description="" },
+                new CityDto()
+                {Id=1,Name="Moscow",Description="",
+                    PointOfIntrests=new List<PointOfIntrestDto>()
+                    {
+                    new PointOfIntrestDto() { id=1,Name="meydansorkh" ,Description=""},
+                    new PointOfIntrestDto() {id=2,Name="moscow city" ,Description=""}
+                    } 
+                },
+                new CityDto() {Id=2,Name="Tehran",Description="" 
+                ,PointOfIntrests=new List<PointOfIntrestDto>()
+                {
+                    new PointOfIntrestDto() {id=3,Name="milad" ,Description=""}
+                }
+                },
                 new CityDto() {Id=3,Name="Room",Description="" }
 
             };
---
+++ CityInfo.API/Models/PointOfIntrestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CityInfo.API.Models
+{
+    public class PointOfIntrestDto
+    {
+        public int id { get; set; }
+        public string Name  { get; set; }=string.Empty;
+        public string? Description { get; set; }
+
+    }
+}
