2189e88e5baee3c40523ca65d3295dda9bbb1fa3
alireza asgary <asgary_alireza@hotmail.com>
2023-01-07 23:12:48 +0300

    add token

---
+++ CityInfo.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,28 @@
+﻿using CityInfo.API.Models_Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        public class AuthenticationRequerstBody
+        {
+            public string? UserName { get; set; }
+            public string? Password { get; set; }
+        }
+        [HttpPost("Authenticate")]
+        public ActionResult<string> Authenticate(AuthenticationRequerstBody user)
+        {
+            var Validateuser = ValidatUserCeridential(user.UserName, user.Password);
+            return "";
+        }
+
+        private CityInfoUser ValidatUserCeridential(string userName,string password)
+        {
+            return null;
+        }
+    }
+}
